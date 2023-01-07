91b07a5f455d07c9d8c343de026bfd3890bfbfd4
lienism <didwhdqls7@gmail.com>
2023-01-07 12:52:39 +0900

    first

---
+++ Matchshape_all_product_picture_process/Matchshape_all_product_process_form.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Matchshape_all_product_picture_process
+{
+    partial class Matchshape_all_product_process_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.file_path_button = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.X1_textbox = new System.Windows.Forms.TextBox();
+            this.Y1_textbox = new System.Windows.Forms.TextBox();
+            this.Y2_textbox = new System.Windows.Forms.TextBox();
+            this.X2_textbox = new System.Windows.Forms.TextBox();
+            this.X1_label = new System.Windows.Forms.Label();
+            this.Y1_label = new System.Windows.Forms.Label();
+            this.Y2_label = new System.Windows.Forms.Label();
+            this.X2_label = new System.Windows.Forms.Label();
+            this.Crop_checkbox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // file_path_button
+            // 
+            this.file_path_button.Location = new System.Drawing.Point(129, 55);
+            this.file_path_button.Name = "file_path_button";
+            this.file_path_button.Size = new System.Drawing.Size(75, 23);
+            this.file_path_button.TabIndex = 0;
+            this.file_path_button.Text = "file_path";
+            this.file_path_button.UseVisualStyleBackColor = true;
+            this.file_path_button.Click += new System.EventHandler(this.file_path_button_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // X1_textbox
+            // 
+            this.X1_textbox.Location = new System.Drawing.Point(63, 107);
+            this.X1_textbox.Name = "X1_textbox";
+            this.X1_textbox.Size = new System.Drawing.Size(100, 19);
+            this.X1_textbox.TabIndex = 1;
+            // 
+            // Y1_textbox
+            // 
+            this.Y1_textbox.Location = new System.Drawing.Point(217, 107);
+            this.Y1_textbox.Name = "Y1_textbox";
+            this.Y1_textbox.Size = new System.Drawing.Size(100, 19);
+            this.Y1_textbox.TabIndex = 2;
+            // 
+            // Y2_textbox
+            // 
+            this.Y2_textbox.Location = new System.Drawing.Point(217, 140);
+            this.Y2_textbox.Name = "Y2_textbox";
+            this.Y2_textbox.Size = new System.Drawing.Size(100, 19);
+            this.Y2_textbox.TabIndex = 4;
+            // 
+            // X2_textbox
+            // 
+            this.X2_textbox.Location = new System.Drawing.Point(63, 140);
+            this.X2_textbox.Name = "X2_textbox";
+            this.X2_textbox.Size = new System.Drawing.Size(100, 19);
+            this.X2_textbox.TabIndex = 3;
+            // 
+            // X1_label
+            // 
+            this.X1_label.AutoSize = true;
+            this.X1_label.Location = new System.Drawing.Point(12, 107);
+            this.X1_label.Name = "X1_label";
+            this.X1_label.Size = new System.Drawing.Size(24, 12);
+            this.X1_label.TabIndex = 5;
+            this.X1_label.Text = "X1 :";
+            // 
+            // Y1_label
+            // 
+            this.Y1_label.AutoSize = true;
+            this.Y1_label.Location = new System.Drawing.Point(176, 107);
+            this.Y1_label.Name = "Y1_label";
+            this.Y1_label.Size = new System.Drawing.Size(24, 12);
+            this.Y1_label.TabIndex = 6;
+            this.Y1_label.Text = "Y1 :";
+            // 
+            // Y2_label
+            // 
+            this.Y2_label.AutoSize = true;
+            this.Y2_label.Location = new System.Drawing.Point(176, 147);
+            this.Y2_label.Name = "Y2_label";
+            this.Y2_label.Size = new System.Drawing.Size(24, 12);
+            this.Y2_label.TabIndex = 8;
+            this.Y2_label.Text = "Y2 :";
+            // 
+            // X2_label
+            // 
+            this.X2_label.AutoSize = true;
+            this.X2_label.Location = new System.Drawing.Point(12, 147);
+            this.X2_label.Name = "X2_label";
+            this.X2_label.Size = new System.Drawing.Size(24, 12);
+            this.X2_label.TabIndex = 7;
+            this.X2_label.Text = "X2 :";
+            // 
+            // Crop_checkbox
+            // 
+            this.Crop_checkbox.AutoSize = true;
+            this.Crop_checkbox.Location = new System.Drawing.Point(14, 59);
+            this.Crop_checkbox.Name = "Crop_checkbox";
+            this.Crop_checkbox.Size = new System.Drawing.Size(48, 16);
+            this.Crop_checkbox.TabIndex = 9;
+            this.Crop_checkbox.Text = "Crop";
+            this.Crop_checkbox.UseVisualStyleBackColor = true;
+            // 
+            // Matchshape_all_product_process_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 182);
+            this.Controls.Add(this.Crop_checkbox);
+            this.Controls.Add(this.Y2_label);
+            this.Controls.Add(this.X2_label);
+            this.Controls.Add(this.Y1_label);
+            this.Controls.Add(this.X1_label);
+            this.Controls.Add(this.Y2_textbox);
+            this.Controls.Add(this.X2_textbox);
+            this.Controls.Add(this.Y1_textbox);
+            this.Controls.Add(this.X1_textbox);
+            this.Controls.Add(this.file_path_button);
+            this.Name = "Matchshape_all_product_process_form";
+            this.Text = "Matchshape_all_product_process_form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button file_path_button;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox X1_textbox;
+        private System.Windows.Forms.TextBox Y1_textbox;
+        private System.Windows.Forms.TextBox Y2_textbox;
+        private System.Windows.Forms.TextBox X2_textbox;
+        private System.Windows.Forms.Label X1_label;
+        private System.Windows.Forms.Label Y1_label;
+        private System.Windows.Forms.Label Y2_label;
+        private System.Windows.Forms.Label X2_label;
+        private System.Windows.Forms.CheckBox Crop_checkbox;
+    }
+}
+
---
+++ Matchshape_all_product_picture_process/Matchshape_all_product_process_form.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+
+namespace Matchshape_all_product_picture_process
+{
+    public partial class Matchshape_all_product_process_form : Form
+    {
+        String open_file_path;
+        Mat Crop_result;
+        Mat RGB_result;
+
+        
+        public Matchshape_all_product_process_form()
+        {
+            InitializeComponent();
+        }
+        List<Mat> mats;
+        private void file_path_button_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            open_file_path = getDirectory(openFileDialog1.FileName, openFileDialog1.SafeFileName);
+            DirectoryInfo Original_Directory = new DirectoryInfo(open_file_path);
+            FileInfo[] Files = Original_Directory.GetFiles();
+
+            mats = new List<Mat>();
+            
+            for(int z = 0; z < Files.Length; z++)
+            {
+                mats.Add(Cv2.ImRead(Files[z].FullName));
+            }
+
+            Mat Case_Image_left;
+            Mat Case_Image_right;
+            OpenCvSharp.Point[][] result_left_contour;
+            HierarchyIndex[] result_left_hierachy;
+            OpenCvSharp.Point[][] result_right_contour;
+            HierarchyIndex[] result_right_hierachy;
+            OpenCvSharp.Point[] Sort_result_left_contour;
+            OpenCvSharp.Point[] Sort_result_right_contour;
+            int X1 = Convert.ToInt32(X1_textbox.Text);
+            int Y1 = Convert.ToInt32(Y1_textbox.Text);
+            int X2 = Convert.ToInt32(X2_textbox.Text);
+            int Y2 = Convert.ToInt32(Y2_textbox.Text);
+            String[] CSV_Left_Result;
+            String[] CSV_Right_Result;
+            Double result;
+
+
+            using (StreamWriter file = new StreamWriter(open_file_path + "result.csv", false, Encoding.GetEncoding("UTF-8")))
+            {
+                file.WriteLine("画像1, 前後 , 角度 , 明るさ , 画像2, 前後 , 角度 , 明るさ , 比較結果");
+                for (int i = 0; i < Files.Length; i++)
+                {
+                    for (int j = i; j < Files.Length; j++)
+                    {
+                        if ((Path.GetExtension(Files[i].FullName) != ".png" && Path.GetExtension(Files[i].FullName) != ".jpg")
+                       || (Path.GetExtension(Files[j].FullName) != ".png" && Path.GetExtension(Files[j].FullName) != ".jpg"))
+                        {
+                            continue;
+                        }
+                        Debug.WriteLine(i + " i : " + Files[i].FullName);
+                        Debug.WriteLine(j + " j : " + Files[j].FullName);
+                        //Mat Case_Image_left = Cv2.ImRead(Files[i].FullName);
+                        //Mat Case_Image_right = Cv2.ImRead(Files[j].FullName);
+                        if(Crop_checkbox.Checked)
+                        {
+                            Case_Image_left = Crop(mats[i], X1, Y1, X2, Y2);
+                            Case_Image_right = Crop(mats[j], X1, Y1, X2, Y2);
+                        }
+                        else
+                        {
+                            Case_Image_left = mats[i];
+                            Case_Image_right = mats[j];
+                        }
+                        
+                        Case_Image_left = RGB_Process(Case_Image_left);
+                        Case_Image_right = RGB_Process(Case_Image_right);
+                        
+                        Cv2.ImWrite(Files[i].FullName.Replace(Path.GetExtension(Files[i].FullName), "_1"+ Path.GetExtension(Files[i].FullName)), Case_Image_left);
+                        Cv2.CvtColor(Case_Image_left, Case_Image_left, ColorConversionCodes.BGR2GRAY);
+                        Cv2.CvtColor(Case_Image_right, Case_Image_right, ColorConversionCodes.BGR2GRAY);
+                        Cv2.Threshold(Case_Image_left, Case_Image_left, 1, 255, ThresholdTypes.Binary);
+                        Cv2.Threshold(Case_Image_right, Case_Image_right, 1, 255, ThresholdTypes.Binary);
+
+                       
+                        Cv2.FindContours(Case_Image_left, out result_left_contour, out result_left_hierachy, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+                        Cv2.FindContours(Case_Image_right, out result_right_contour, out result_right_hierachy, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+                        Sort_result_left_contour = Contour_Max_sort(result_left_contour);
+                        Sort_result_right_contour = Contour_Max_sort(result_right_contour);
+                        result = Cv2.MatchShapes(Sort_result_left_contour, Sort_result_right_contour, ShapeMatchModes.I1);
+                        CSV_Left_Result = CSV_information(Files[i].Name);
+                        CSV_Right_Result = CSV_information(Files[j].Name);
+                        file.WriteLine(CSV_Left_Result[0] + "," + CSV_Left_Result[1] + "," + CSV_Left_Result[2] + "," + CSV_Left_Result[3] + ","
+                            + CSV_Right_Result[0] + "," + CSV_Right_Result[1] + "," + CSV_Right_Result[2] + "," + CSV_Right_Result[3] + "," + result);
+                        
+                        Case_Image_left.Dispose();
+                        Case_Image_right.Dispose();
+                    }
+                }
+            }
+        }
+
+        String[] Token;
+        public String[] CSV_information(String picture_name)
+        {
+            Token = picture_name.Split('_');
+            switch(Token[1])
+            {
+                case "F":
+                    Token[1] = "前面";
+                    break;
+                case "B":
+                    Token[1] = "後面";
+                    break;
+            }
+            Token[3].Replace(Path.GetExtension(picture_name), "");
+            return Token;
+        }
+        public Mat Crop(Mat src , int x1 , int y1 , int x2 , int y2)
+        {
+            Crop_result = new Mat(src, new Rect(x1, y1, x2 - x1, y2 - y1));
+            return Crop_result;
+        }
+        public OpenCvSharp.Point[] Contour_Max_sort(OpenCvSharp.Point[][] contour)
+        {
+            for(int a = 0; a < contour.Length; a++)
+            {
+                if(contour[0].Length < contour[a].Length)
+                {
+                    contour[0] = contour[a];
+                }
+            }
+            return contour[0];
+        }
+        public String getDirectory(String fullPath ,String fileName)
+        {
+            return fullPath.Replace(fileName, "");
+        }
+
+
+        Mat Dummy_mat = new Mat();
+        OpenCvSharp.Point[][] Blue;
+        OpenCvSharp.Point[][] Green;
+        OpenCvSharp.Point[][] Red;
+        OpenCvSharp.Point[][] Dummy;
+
+        HierarchyIndex[] blue_hi;
+        HierarchyIndex[] green_hi;
+        HierarchyIndex[] red_hi;
+        Mat[] BGR;
+        Mat filter = new Mat();
+        double Blue_sum;
+        double Green_sum;
+        double Red_sum;
+        public Mat RGB_Process(Mat src)
+        {
+            RGB_result = new Mat();
+            BGR = Cv2.Split(src);
+            for (int i = 0; i < BGR.Length; i++)
+            {
+                Cv2.CvtColor(BGR[i], BGR[i], ColorConversionCodes.GRAY2BGR);
+                Cv2.CvtColor(BGR[i], BGR[i], ColorConversionCodes.BGR2GRAY);
+                Cv2.Threshold(BGR[i], BGR[i], 0, 255, ThresholdTypes.Otsu | ThresholdTypes.BinaryInv);
+                for (int a = 0; a <= 6; a++)
+                {
+                    Cv2.Erode(BGR[i], BGR[i], filter);
+                    Cv2.Dilate(BGR[i], BGR[i], filter);
+                }
+            }
+            Cv2.FindContours(BGR[0], out Blue, out blue_hi, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+            Cv2.FindContours(BGR[1], out Green, out green_hi, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+            Cv2.FindContours(BGR[2], out Red, out red_hi, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+            Blue_sum = 0;
+            for (int i = 0; i < Blue.Length; i++)
+            {
+                Blue_sum = Blue_sum + Cv2.ContourArea(Blue[i]);
+            }
+            Green_sum = 0;
+            for (int i = 0; i < Green.Length; i++)
+            {
+                Green_sum = Green_sum + Cv2.ContourArea(Green[i]);
+            }
+            Red_sum = 0;
+            for (int i = 0; i < Red.Length; i++)
+            {
+                Red_sum = Red_sum + Cv2.ContourArea(Red[i]);
+            }
+            if (Blue_sum < Red_sum)
+            {
+                Dummy = Blue;
+                Dummy_mat = BGR[0];
+                Blue = Red;
+                BGR[0] = BGR[2];
+                Red = Dummy;
+                BGR[2] = Dummy_mat;
+            }
+            if (Blue_sum < Green_sum)
+            {
+                Dummy = Blue;
+                Dummy_mat = BGR[0];
+                Blue = Green;
+                Dummy_mat = BGR[1];
+                Green = Dummy;
+                BGR[1] = Dummy_mat;
+            }
+            if (Red_sum < Green_sum)
+            {
+                Dummy = Green;
+                Dummy_mat = BGR[1];
+                Green = Red;
+                BGR[1] = BGR[2];
+                Red = Dummy;
+                BGR[1] = Dummy_mat;
+            }
+
+            Cv2.BitwiseOr(BGR[0], BGR[1], RGB_result);
+            Cv2.BitwiseAnd(RGB_result, BGR[2], RGB_result);
+            Cv2.BitwiseAnd(RGB_result.CvtColor(ColorConversionCodes.GRAY2BGR), src, RGB_result);
+
+            foreach (var item in BGR)
+            {
+                item.Dispose();
+            }
+            BGR = null;
+            
+            return RGB_result;
+        }
+    }
+}
